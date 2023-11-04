ad1b62702a5883ac1ac0fd83e2feeff09b6f97a8
Oleg Didenko <diaglyonok@yandex.ru>
2023-11-05 00:14:35 +0600

    list ext update

--- lib/extensions/list_ext.dart
+++ lib/extensions/list_ext.dart
@@ -1,31 +1,30 @@
 extension ListExtNullable<E> on List<E>? {
   bool isNullOrEmpty() => (this == null) || this!.isEmpty;
   bool isNotNullOrEmpty() => !isNullOrEmpty();
-  List<E>? getSorted<E>([int? Function(E, E)? compare]) {
-    if (this == null) {
-      return null;
-    }
+}
 
-    List<E> newList = this!.map((e) => e).toList().cast<E>();
-    if (compare == null && E is Comparable) {
-      newList.sort();
-    } else if (compare != null) {
-      newList.sort(compare as int Function(E?, E?)?);
-    }
-    return newList;
-  }
-
-  List<E>? copy() => this == null ? null : this!.map((e) => e).toList();
+extension ListExtNonNullable<E> on List<E> {
+  List<E>? copy() => map((e) => e).toList();
 
   bool indexValid(int index) {
     if (isNullOrEmpty()) {
       return false;
     }
 
-    if (index >= this!.length || index < 0) {
+    if (index >= length || index < 0) {
       return false;
     }
 
     return true;
+  }
+
+  List<E> getSorted([int? Function(E, E)? compare]) {
+    List<E> newList = map((e) => e).toList().cast<E>();
+    if (compare == null && E is Comparable) {
+      newList.sort();
+    } else if (compare != null) {
+      newList.sort(compare as int Function(E?, E?)?);
+    }
+    return newList;
   }
 }
